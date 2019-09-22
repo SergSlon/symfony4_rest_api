011e1b5e8017c2ea7a707d1c82beed33b95ac0c7
Liamin Serhii <liamin.web@gmail.com>
2019-09-22 14:27:50 +0300

    composer require generator

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
