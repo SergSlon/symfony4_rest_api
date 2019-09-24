160f02ab75b331e1e8125173554ba5d23bd82661
Sergey LIAMIN <sergey.liamin@amadeus.com>
2019-09-24 19:58:36 +0300

    add twig

--- src/Controller/ClassroomsController.php
+++ src/Controller/ClassroomsController.php
@@ -2,21 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Classroom;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 
 class ClassroomsController extends AbstractFOSRestController
 {
     /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository
+     */
+    private function getClassroomsRepository()
+    {
+        return $this->getDoctrine()->getRepository(Classroom::class);
+    }
+
+    /**
      * @Rest\Get("/classrooms", name="classrooms")
      */
     public function getAction()
     {
-        return $this->json(
-            [
-                'message' => 'Welcome to your new controller!',
-                'path' => 'src/Controller/ClassroomsController.php',
-            ]
-        );
+        $classrooms = $this->getClassroomsRepository()->findAll();
+
+        $view = $this->view($classrooms)
+            ->setTemplate('classrooms/all.html.twig')
+            ->setTemplateVar('classrooms')
+        ;
+
+        return $this->handleView($view);
     }
 }
